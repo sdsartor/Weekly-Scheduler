ebb0f91ed008a798766c8a5060c04005e535947f
sdsartor <sdsartor48@gmail.com>
2023-01-24 18:37:15 -0800

    Everything is complete aside from the readme file which i plan to do tommorow before class begins.  The javascripts actually works after I placed the hours by the date and removed the dashes from the hour ids. I added a decent background to make mine unique and also increased the roundness of the times and save buttons.

--- Develop/script.js
+++ Develop/script.js
@@ -1,59 +1,66 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(window).load(function(){
-
-});
-var todayDate = moment().format('dddd, MMM Do YYYY');
-$("#currentDay").html(todayDate);
 
 $(document).ready(function () {
-  $(".saveBtn").on('click', function() {
-  var text = $(this).child(".descriptive").val();
-  var time = $(this).parent().attr("id");
-localStorage.setItem(time, text);
-});
-function timeTracker() {
-  //get current number of hours.
-  var timeNow = moment().hour();
+  
+  $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm A"));
 
-  // loop over time blocks
-  $(".time-block").each(function () {
-      var blockTime = parseInt($(this).attr("id").split("hour")[1]);
+  $(".saveBtn").on("click", function () {
+  
+      console.log(this);
+      var textlog = $(this).siblings(".description").val();
+      var timetracker = $(this).parent().attr("id"); 
 
-      // To check the time and add the classes for background indicators
-      if (blockTime < timeNow) {
-          $(this).removeClass("future");
-          $(this).removeClass("present");
-          $(this).addClass("past");
-      }
-      else if (blockTime === timeNow) {
-          $(this).removeClass("past");
-          $(this).removeClass("future");
-          $(this).addClass("present");
-      }
-      else {
-          $(this).removeClass("present");
-          $(this).removeClass("past");
-          $(this).addClass("future");
-
-      }
+      localStorage.setItem(timetracker, textlog);
   })
-}
+  
+  $("#hour8 .description").val(localStorage.getItem("hour8"));
+  $("#hour9 .description").val(localStorage.getItem("hour9"));
+  $("#hour10 .description").val(localStorage.getItem("hour10"));
+  $("#hour11 .description").val(localStorage.getItem("hour11"));
+  $("#hour12 .description").val(localStorage.getItem("hour12"));
+  $("#hour13 .description").val(localStorage.getItem("hour13"));
+  $("#hour14 .description").val(localStorage.getItem("hour14"));
+  $("#hour15 .description").val(localStorage.getItem("hour15"));
+  $("#hour16 .description").val(localStorage.getItem("hour16"));
+  $("#hour17 .description").val(localStorage.getItem("hour17"));
+  $("#hour18 .description").val(localStorage.getItem("hour18"));
+  $("#hour19 .description").val(localStorage.getItem("hour19"));
+  $("#hour20 .description").val(localStorage.getItem("hour20"));
+  $("#hour21 .description").val(localStorage.getItem("hour21"));
+  $("#hour22 .description").val(localStorage.getItem("hour22"));
+  $("#hour23 .description").val(localStorage.getItem("hour23"));
+  $("#hour24 .description").val(localStorage.getItem("hour24"));
 
-// Get item from local storage if any
-$("#hour-8 .description").val(localStorage.getItem("hour8"));
-$("#hour-9 .description").val(localStorage.getItem("hour9"));
-$("#hour-10 .description").val(localStorage.getItem("hour10"));
-$("#hour-11 .description").val(localStorage.getItem("hour11"));
-$("#hour-12 .description").val(localStorage.getItem("hour12"));
-$("#hour-13 .description").val(localStorage.getItem("hour13"));
-$("#hour-14 .description").val(localStorage.getItem("hour14"));
-$("#hour-15 .description").val(localStorage.getItem("hour15"));
-$("#hour-16 .description").val(localStorage.getItem("hour16"));
-$("#hour-17 .description").val(localStorage.getItem("hour17"));
 
-timeTracker();
+  function hourTracker() {
+ 
+      var currentHour = moment().hour();
+
+     
+      $(".time-block").each(function () {
+          var blockHour = parseInt($(this).attr("id").split("hour")[1]);
+          console.log( blockHour, currentHour)
+
+       
+          if (blockHour < currentHour) {
+              $(this).addClass("past");
+              $(this).removeClass("future");
+              $(this).removeClass("present");
+          }
+          else if (blockHour === currentHour) {
+              $(this).removeClass("past");
+              $(this).addClass("present");
+              $(this).removeClass("future");
+          }
+          else {
+              $(this).removeClass("present");
+              $(this).removeClass("past");
+              $(this).addClass("future");
+          }
+      })
+  }
+  hourTracker();
+})
+
 
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -73,4 +80,3 @@ timeTracker();
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
-});
